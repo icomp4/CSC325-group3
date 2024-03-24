d72f76e94a98ff31d41481a5d58d17e4792c9aaa
isaiah compere <rewrittenzip@gmail.com>
2024-03-24 14:56:31 -0400

    init firebase

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.example.demo1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires firebase.admin;
+    requires com.google.auth.oauth2;
 
 
     opens org.example.demo1 to javafx.fxml;
--- src/main/java/org/example/demo1/ShadyAuto.java
+++ src/main/java/org/example/demo1/ShadyAuto.java
@@ -4,13 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import com.google.firebase.*;
+import com.google.auth.oauth2.GoogleCredentials;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class ShadyAuto extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ShadyAuto.class.getResource("hello-view.fxml"));
+        FileInputStream serviceAccount =
+                new FileInputStream("key.jason");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(ShadyAuto.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("shadyAuto");
         stage.setScene(scene);
@@ -18,6 +29,7 @@ public class ShadyAuto extends Application {
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }
