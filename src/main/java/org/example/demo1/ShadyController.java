cd0693768ace51fe9a4e6488bfa14520ff157325
isaiah compere <rewrittenzip@gmail.com>
2024-03-24 14:36:09 -0400

    refactor

--- src/main/java/org/example/demo1/ShadyController.java
+++ src/main/java/org/example/demo1/ShadyController.java
@@ -3,7 +3,7 @@ package org.example.demo1;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ShadyController {
     @FXML
     private Label welcomeText;
 
